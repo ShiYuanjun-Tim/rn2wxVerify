5e7837372b218f6807dba916fe083b933af25aad
Timshi <syj_news@163.com>
2019-01-20 22:56:16 +0800

    update

--- src/page/comptests/adapter/scrollview.js
+++ src/page/comptests/adapter/scrollview.js
@@ -4,6 +4,12 @@ import { _alert } from "../../utils"
 
 export default class Page extends Component {
 
+  constructor(prop){
+    super(prop)
+    this.state = {
+      scVal:0
+    }
+  }
   myscrollHandler(e) {
     console.log('onScroll',e.nativeEvent || e.detail)
   }
@@ -12,14 +18,19 @@ export default class Page extends Component {
      
      this.scrollV.scrollTo({x:100,y:100})
   }
+  scrollTo2 =(e) =>{
+     this.refs.sv2.scrollTo({x:100,y:100})
+  }
 
+  _scroll  = (v) =>{
+    this.setState({scVal: v})
+  }
   render() {
     return <View style={{flex:1, paddingTop: 40}}>
       <View><Text>1 vetical</Text></View> 
-      <TouchableOpacity onPress={this.scrollTo}><Text>rn scroll to 100</Text></TouchableOpacity>
-      <TouchableOpacity onPress={this.scrollTo}><Text>wx scroll to 100</Text></TouchableOpacity>
-
-      <ScrollView    style={{ flex:1,  height:250}} 
+      <TouchableOpacity onPress={this.scrollTo}><Text> scroll to 100</Text></TouchableOpacity>
+ 
+      <ScrollView    style={{ flex:1,  height:250}}   
       bounces={false}  ref={(ref)=>{
         debugger
         this.scrollV = ref}}
@@ -36,7 +47,10 @@ export default class Page extends Component {
      </ScrollView>
 
      <Text>2 horizontal</Text>
-      <ScrollView  horizontal  bounces={false} onScroll={this.myscrollHandler}
+     <TouchableOpacity onPress={this.scrollTo2}><Text>rn scroll to 100</Text></TouchableOpacity>
+      <TouchableOpacity onPress={this.scrollTo2}><Text>wx scroll to 100</Text></TouchableOpacity>
+
+      <ScrollView  horizontal  bounces={false} onScroll={this.myscrollHandler} ref="sv2"
       contentContainerStyle = {{ backgroundColor:"gold" , height: 100}}>
        <View style={{backgroundColor:"red", width:70, height:70 }}></View>
       <View style={{backgroundColor:"green", width:70, height:70}}></View>
