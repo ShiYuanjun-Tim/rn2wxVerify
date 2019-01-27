15a0acd9457c7cb21b17280482e0768b256c0772
Timshi <syj_news@163.com>
2019-01-27 23:25:32 +0800

    some change

--- src/page/catalogHomeExample/data.js
+++ src/page/catalogHomeExample/data.js
@@ -1,4 +1,4 @@
-import Sign from "../sign"
+// import Sign from "../sign"
 import 'whatwg-fetch'
 
 const parentCatalogList= "https://mapi.wanlitong.com/mobileapi/m2s/catalog/parentCatalogList.do"
@@ -44,7 +44,7 @@ if(process.env.TARO_ENV === 'weapp') {
     return new Promise((res,rej)=>{
 
       data = Object.assign({},temp,data,{reqTime: Date.now()})
-      data.sign = Sign(data)
+      // data.sign = Sign(data)
 
       wx.request({
         url: parentCatalogList,
@@ -64,7 +64,7 @@ if(process.env.TARO_ENV === 'weapp') {
 
 export function getParentCatalogList(){
 
-  return fetch(parentCatalogList , {
+  /* return fetch(parentCatalogList , {
     method: 'POST',
     headers:{
       ['content-type']: 'application/x-www-form-urlencoded'
@@ -87,5 +87,11 @@ export function getParentCatalogList(){
     return data.json()
   }).catch(err=>{
     console.log('fail',err)
-  })
+  }) */
+
+return Promise.resolve({"body":{"compress":false,"encrypt":false,"message":"OK","mktEndDate":0,"mktPrice":false,"mktStartDate":0,"result":{"catalogList":[{"id":"2858","highLightCatalog":false,"catalogName":"热门推荐"},{"id":"1001","highLightCatalog":false,"catalogName":"手机数码"},{"id":"1002","highLightCatalog":false,"catalogName":"电脑/办公"},{"id":"1003","highLightCatalog":false,"catalogName":"家用电器"},{"id":"1004","highLightCatalog":false,"catalogName":"家居/家纺"},{"id":"3007","highLightCatalog":false,"catalogName":"服饰鞋帽"},{"id":"1006","highLightCatalog":false,"catalogName":"运动/户外"},{"id":"1005","highLightCatalog":false,"catalogName":"礼品/箱包"},{"id":"1007","highLightCatalog":false,"catalogName":"食品/酒水"},{"id":"1008","highLightCatalog":false,"catalogName":"美妆/个护"},{"id":"1009","highLightCatalog":false,"catalogName":"钟表/首饰"},{"id":"1010","highLightCatalog":false,"catalogName":"母婴用品"},{"id":"1011","highLightCatalog":false,"catalogName":"汽车用品"},{"id":"1012","highLightCatalog":false,"catalogName":"电子票券"}]},"statusCode":"0000"},"head":{"custString":"1.36","msgVersion":"3.6.1","reqAppId":"ios_app_wanlitong","reqTime":"1548511623711","rspCode":"0","rspDescription":"安全验证通过","rspTime":"1548511627044"}})
+.then(raw=>{
+  return raw.body.result.catalogList
+})
+
 }
--- src/page/catalogHomeExample/index.js
+++ src/page/catalogHomeExample/index.js
@@ -1,26 +1,38 @@
 
-import  * as   React from 'react';
+import * as React  from 'react';
 import {  View ,Text, TouchableOpacity, ScrollView, Image , 
   Dimensions,
   PixelRatio,
   Platform,
-   
+  StyleSheet,
 } from 'react-native';
 import {getParentCatalogList} from './data'
+import {_alert} from './../utils'
 
 const {height,width} = Dimensions.get('window')
+debugger
 export default class Page extends React.Component {
 
   constructor(props) {
-    super(props)
-    
+    super(props);
+    this.state = {
+      catalog:[],
+      selected:null
+    }
   }
   config = {
     navigationBarTitleText: '组件适配',
   }
 
   componentDidMount(){
-    getParentCatalogList()
+     getParentCatalogList().then(data=>{
+       console.log("getParentCatalogList done ")
+       const selected = this.state.selected
+       this.setState({
+        catalog:data,
+        selected: selected || data[0].id
+       })
+     }).catch(err=>{debugger})
 
     if (process.env.TARO_ENV === 'weapp'){
       console.log(wx.getSystemInfoSync())
@@ -45,9 +57,14 @@ export default class Page extends React.Component {
   
       return (
        <View flexContainer style={{flex:1,justifyContent:'center', alignItems:"center", flexDirection:'row' , backgroundColor:'gold', height }}>
-         <View flexContainer style={{backgroundColor:'blue', width:86 , height:'100%'}}>
+         <View flexContainer style={{backgroundColor:'blue', width:86 , height:'100%',}}>
            <ScrollView style={{ flex:1}} contentContainerStyle={{backgroundColor:'green'}}>
-
+            {
+              this.state.catalog.map((data , i)=>{
+                const sty = data.id == this.state.selected ? styles.selected :{};
+                 return <View key={data.id} style={[styles.cata,sty]}><Text>{data.catalogName}</Text></View> 
+              })
+            }
            </ScrollView>
          </View>
 
@@ -60,3 +77,12 @@ export default class Page extends React.Component {
   }
 }
 
+const styles = (StyleSheet ).create({
+  cata:{
+    height:70,
+    width:40
+  },
+  selected:{
+    color: 'red'
+  }
+})
